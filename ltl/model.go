ec5cdfa2856f6731d4c9e8344e0f80500a9d4511
Paul van Brouwershaven <paul.vanbrouwershaven@entrust.com>
2021-07-20 14:17:10 +0200

    Add JSON tags

--- ltl/model.go
+++ ltl/model.go
@@ -5,66 +5,66 @@ import (
 )
 
 type PKI struct {
-	Name        string `yaml:"name" validate:"required"`
-	Description string `yaml:"description"`
+	Name        string `json:"name" yaml:"name" validate:"required"`
+	Description string `json:"description" yaml:"description"`
 
 	// A link to the trusted list program or vendor/product when no such
 	// dedicated website exists.
-	Website string `yaml:"website" validate:"required,url"`
+	Website string `json:"website" yaml:"website" validate:"required,url"`
 
 	// Email or website address to contact privately
-	Contact string `yaml:"contact" validate:"omitempty,email|url"`
+	Contact string `json:"contact" yaml:"contact" validate:"omitempty,email|url"`
 
 	// Using the Common CA Database
-	CCADB bool `yaml:"ccadb"`
+	CCADB bool `json:"ccadb" yaml:"ccadb"`
 
 	// Legal context
-	LegalContext string `yaml:"legal-context"`
+	LegalContext string `json:"legal-context" yaml:"legal-context"`
 
 	// How is compliance audited
-	Audit []Audit `yaml:"audit" validate:"dive"`
+	Audit []Audit `json:"audit" yaml:"audit" validate:"dive"`
 
-	Requirements []Source   `yaml:"requirements" validate:"dive"`
-	Discussions  []Source   `yaml:"discussions" validate:"dive"`
-	Issues       []Source   `yaml:"issues" validate:"dive"`
-	TrustLists   TrustLists `yaml:"trust-lists"`
+	Requirements []Source   `json:"requirements" yaml:"requirements" validate:"dive"`
+	Discussions  []Source   `json:"discussions" yaml:"discussions" validate:"dive"`
+	Issues       []Source   `json:"issues" yaml:"issues" validate:"dive"`
+	TrustLists   TrustLists `json:"trust-lists" yaml:"trust-lists"`
 }
 
 // Audit specifies the audit options and or requirements,
 type Audit struct {
-	Name        string        `yaml:"name" validate:"required"`
-	Description string        `yaml:"description"`
-	Frequency   time.Duration `yaml:"frequency"`
-	Policies    []string      `yaml:"policies"`
+	Name        string        `json:"name" yaml:"name" validate:"required"`
+	Description string        `json:"description" yaml:"description"`
+	Frequency   time.Duration `json:"frequency" yaml:"frequency"`
+	Policies    []string      `json:"policies" yaml:"policies"`
 }
 
 // TrustLists under the same PKI program.
 // If one list has multiple distinct programs, create a new file for each.
 type TrustLists struct {
-	Info   string `yaml:"info"`
-	Policy string `yaml:"policy"`
+	Info   string `json:"info" yaml:"info"`
+	Policy string `json:"policy" yaml:"policy"`
 
-	Trust []Trust `yaml:"trust" validate:"omitempty,dive"`
+	Trust []Trust `json:"trust" yaml:"trust" validate:"omitempty,dive"`
 }
 
 // Trust links to a file containing the CA certificates trusted by this PKI
 // for the given purpose. A file might be referenced in multiple formats, the
 // preferred format (type) is PEM.
 type Trust struct {
-	Purposes []string `yaml:"purposes"`
+	Purposes []string `json:"purposes" yaml:"purposes"`
 
 	// When a program has trust lists for different purposes, there can be
 	// generic requirements and requirements that are specific to one purpose.
-	Requirements []Source `yaml:"requirements" validate:"dive"`
-	Discussions  []Source `yaml:"discussions" validate:"dive"`
-	Issues       []Source `yaml:"issues" validate:"dive"`
-	Audit        []Audit  `yaml:"audit" validate:"dive"`
+	Requirements []Source `json:"requirements" yaml:"requirements" validate:"dive"`
+	Discussions  []Source `json:"discussions" yaml:"discussions" validate:"dive"`
+	Issues       []Source `json:"issues" yaml:"issues" validate:"dive"`
+	Audit        []Audit  `json:"audit" yaml:"audit" validate:"dive"`
 
-	List []Source `yaml:"list" validate:"dive"`
+	List []Source `json:"list" yaml:"list" validate:"dive"`
 }
 
 // Source is a wrapper to allow specifing the type of source file
 type Source struct {
-	Type string `yaml:"type" validate:"omitempty,oneof=HTML RSS ATOM XML JSON PEM PKCS7"`
-	URL  string `yaml:"url" validate:"required,url"`
+	Type string `json:"type" yaml:"type" validate:"omitempty,oneof=HTML RSS ATOM XML JSON PEM PKCS7"`
+	URL  string `json:"url" yaml:"url" validate:"required,url"`
 }
